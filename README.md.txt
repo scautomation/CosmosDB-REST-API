b2b334dec75fa5ccf15d6ea957805ba27cbd3966
scautomation <35973682+scautomation@users.noreply.github.com>
2018-06-27 09:27:49 -0500

    intiial commit
    initial commit for posting to my blog

---
+++ README.md.txt
@@ -0,0 +1,11 @@
+<#
+This script has been modified from the sample provided by Microsoft found here https://gallery.technet.microsoft.com/How-to-list-all-documents-bf6d2101
+
+.Purpose - Post JSON Payload to Cosmos DB via Powershell. Microsoft provided examples in C# and other languages, but only provided query examples and not POST example.
+
+.Requirements - Cosmos DB, URI, Primary Key, Database name, Collection name
+
+Billy York
+@scautomation
+www.systemcenterautomation.com
+#>
---
+++ RESTAPI_Post.ps1
@@ -0,0 +1,76 @@
+﻿<#
+This script has been modified from the sample provided by Microsoft found here https://gallery.technet.microsoft.com/How-to-list-all-documents-bf6d2101
+
+Billy York
+@scautomation
+www.systemcenterautomation.com
+#>
+
+# add necessary assembly
+#
+Add-Type -AssemblyName System.Web
+
+# generate authorization key
+Function Generate-MasterKeyAuthorizationSignature
+{
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$true)][String]$verb,
+		[Parameter(Mandatory=$true)][String]$resourceLink,
+		[Parameter(Mandatory=$true)][String]$resourceType,
+		[Parameter(Mandatory=$true)][String]$dateTime,
+		[Parameter(Mandatory=$true)][String]$key,
+		[Parameter(Mandatory=$true)][String]$keyType,
+		[Parameter(Mandatory=$true)][String]$tokenVersion
+	)
+
+	$hmacSha256 = New-Object System.Security.Cryptography.HMACSHA256
+	$hmacSha256.Key = [System.Convert]::FromBase64String($key)
+
+	$payLoad = "$($verb.ToLowerInvariant())`n$($resourceType.ToLowerInvariant())`n$resourceLink`n$($dateTime.ToLowerInvariant())`n`n"
+	$hashPayLoad = $hmacSha256.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($payLoad))
+	$signature = [System.Convert]::ToBase64String($hashPayLoad);
+
+	[System.Web.HttpUtility]::UrlEncode("type=$keyType&ver=$tokenVersion&sig=$signature")
+}
+
+# query
+Function Post-CosmosDb
+{
+	[CmdletBinding()]
+	Param
+	(
+		[Parameter(Mandatory=$true)][String]$EndPoint,
+		[Parameter(Mandatory=$true)][String]$DataBaseId,
+		[Parameter(Mandatory=$true)][String]$CollectionId,
+		[Parameter(Mandatory=$true)][String]$MasterKey,
+		[Parameter(Mandatory=$true)][String]$JSON
+	)
+
+	$Verb = "POST"
+	$ResourceType = "docs";
+	$ResourceLink = "dbs/$DatabaseId/colls/$CollectionId"
+
+	$dateTime = [DateTime]::UtcNow.ToString("r")
+	$authHeader = Generate-MasterKeyAuthorizationSignature -verb $Verb -resourceLink $ResourceLink -resourceType $ResourceType -key $MasterKey -keyType "master" -tokenVersion "1.0" -dateTime $dateTime
+	$header = @{authorization=$authHeader;"x-ms-version"="2017-02-22";"x-ms-date"=$dateTime}
+	$contentType= "application/json"
+	$queryUri = "$EndPoint$ResourceLink/docs"
+
+	$result = Invoke-RestMethod -Method $Verb -ContentType $contentType -Uri $queryUri -Headers $header -Body $JSON
+    return $result.statuscode
+    
+	
+}
+
+# fill the target cosmos database endpoint uri, database id, collection id and masterkey
+$CosmosDBEndPoint = "https://sandlot.documents.azure.com:443/"
+$DatabaseId = "Sandlot"
+$CollectionId = "weather"
+$MasterKey = "G6ybIr3aCU12y3fWNkZKSnEeHB9sYMq08f8fmGrEypxCwgWCryBxhmUsxlwUO5fHXF1GctdsYBNHYPdlynL4rQ=="
+
+
+
+# execute
+Post-CosmosDb -EndPoint $CosmosDBEndPoint -DataBaseId $DataBaseId -CollectionId $CollectionId -MasterKey $MasterKey -JSON $weather
